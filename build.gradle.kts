1d8438edd2c346500211d5cbf2edbbda019682d0
David Burström <david.burstrom@gmail.com>
2022-12-30 15:19:17 +0100

    Release 0.3.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,7 +23,7 @@ configure<SpotlessExtension> {
 allprojects {
     afterEvaluate {
 
-        version = "0.3.0-SNAPSHOT"
+        version = "0.3.0"
         group = "io.github.davidburstrom.gradle.version-compatibility"
 
         apply(plugin = "com.diffplug.spotless")
