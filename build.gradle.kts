4dbe2043b7181328db60ced15e37e09cdf163b51
David Burström <david.burstrom@gmail.com>
2023-01-07 13:22:13 +0100

    Prepare for 0.5.0 development

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,7 +23,7 @@ configure<SpotlessExtension> {
 allprojects {
     afterEvaluate {
 
-        version = "0.4.0"
+        version = "0.5.0-SNAPSHOT"
         group = "io.github.davidburstrom.gradle.version-compatibility"
 
         apply(plugin = "com.diffplug.spotless")
