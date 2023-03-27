801f5ddd96c9545b82ea634137001214596a660e
David Burström <david.burstrom@gmail.com>
2023-03-27 11:17:37 +0200

    Require JDK 11 or later to build the project
    Upcoming versions of build tooling plugins will require it.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -12,5 +12,11 @@ dependencyResolutionManagement {
 
 rootProject.name = "version-compatibility-gradle-plugin"
 
+if (!JavaVersion.current().isJava11Compatible) {
+    throw GradleException(
+        "The project requires JDK 11 or later to build. Current JDK is ${JavaVersion.current()}."
+    )
+}
+
 include(":plugin")
 include(":example")
