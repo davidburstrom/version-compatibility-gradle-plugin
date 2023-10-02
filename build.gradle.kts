c5017843f7216c234d6a34218e0b17a5ab88f934
David Burström <david.burstrom@gmail.com>
2023-10-02 19:30:03 +0200

    Bump KtLint to 1.0.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,7 +11,7 @@ plugins {
 
 val errorProneVersion = "2.22.0"
 val googleJavaFormatVersion = "1.17.0"
-val ktlintVersion = "0.50.0"
+val ktlintVersion = "1.0.0"
 val pitestJUnit5PluginVersion = "1.2.0"
 val pitestMainVersion = "1.15.0"
 val pmdVersion = "6.55.0"
