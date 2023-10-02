851c845172f4156ae0bd4ad54b43b4d5e717b36b
David Burström <david.burstrom@gmail.com>
2023-10-02 19:25:58 +0200

    Bump Error Prone to 2.22.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,7 +9,7 @@ plugins {
     id("net.ltgt.errorprone") version "3.1.0" apply false
 }
 
-val errorProneVersion = "2.21.1"
+val errorProneVersion = "2.22.0"
 val googleJavaFormatVersion = "1.17.0"
 val ktlintVersion = "0.50.0"
 val pitestJUnit5PluginVersion = "1.2.0"
