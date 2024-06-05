64974030abb1b1ad75c8f4636a73b38a5b17f43b
David Burström <david.burstrom@gmail.com>
2024-06-05 18:30:12 +0200

    Fix formatting issue introduced by KtLint 1.3.0

--- example/src/compatLang3Dot5/kotlin/example/KompatArrayUtils3Dot5.kt
+++ example/src/compatLang3Dot5/kotlin/example/KompatArrayUtils3Dot5.kt
@@ -3,7 +3,5 @@ package example
 import org.apache.commons.lang3.ArrayUtils
 
 class KompatArrayUtils3Dot5 : KompatArrayUtils {
-    override fun removeAllOccurrences(array: ByteArray, element: Byte): ByteArray {
-        return ArrayUtils.removeAllOccurences(array, element)
-    }
+    override fun removeAllOccurrences(array: ByteArray, element: Byte): ByteArray = ArrayUtils.removeAllOccurences(array, element)
 }
